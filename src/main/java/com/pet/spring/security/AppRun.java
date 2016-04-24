dceec4f45bd959cd4a6c896b70627b6e146049f4
Oleksandr Ivanov <alex.ivanoff85@gmail.com>
2016-04-24 22:29:34 +0200

    Project initialization

---
+++ src/main/java/com/pet/spring/security/AppRun.java
@@ -0,0 +1,13 @@
+package com.pet.spring.security;
+
+/**
+ * Main class for application running
+ *
+ * @author Oleksandr Ivanov
+ */
+public class AppRun {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/pet/spring/security/SimpleTest.java
@@ -0,0 +1,15 @@
+package com.pet.spring.security;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Simple test example
+ */
+public class SimpleTest {
+
+    @Test
+    public void testCase() {
+        assertTrue(1 == 1);
+    }
+}
