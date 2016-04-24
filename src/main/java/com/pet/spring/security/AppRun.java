96e4dbcb4cd52afcc3433768ae9e92c9f97a2320
Oleksandr Ivanov <alex.ivanoff85@gmail.com>
2016-04-24 23:07:56 +0200

    Added logger and java8 support

--- src/main/java/com/pet/spring/security/AppRun.java
+++ src/main/java/com/pet/spring/security/AppRun.java
@@ -1,5 +1,8 @@
 package com.pet.spring.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Main class for application running
  *
@@ -7,7 +10,15 @@ package com.pet.spring.security;
  */
 public class AppRun {
 
+    private static Logger LOG = LoggerFactory.getLogger(AppRun.class);
+
     public static void main(String[] args) {
+        new Thread(
+                () -> {
+                    LOG.info("Hello from lambdas and Log4j2");
+                }
+        ).start();
+
 
     }
 }
